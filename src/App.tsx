e6899844baba15566c80c90ab3528dd4332e3105
ishantmishra03 <dev.frontend.03@proton.me>
2026-04-06 14:17:29 +0545

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-linear-to-b from-gray-950 to-gray-900 text-white flex flex-col items-center justify-center px-6">
+      <div className="text-center space-y-6">
+        <h1 className="text-4xl md:text-5xl font-bold tracking-tight">
+          React HLS Player
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
